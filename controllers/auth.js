892d025b80e5bbf0721a80ce0f02025172c51cef
TheRealCyberber <shafi.albalushi@gmail.com>
2025-05-05 20:22:32 +0300

    until VIP users added

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,30 +4,69 @@ const bcrypt = require('bcrypt')
 
 const User = require('../models/user.js')
 
-router.get("/sign-up", (req, res) => {
-    res.render('./auth/sign-up.ejs')
+router.get('/sign-up', (req, res) => {
+  res.render('./auth/sign-up.ejs')
 })
 
 router.post('/sign-up', async (req, res) => {
-    try {   
-        const userInDatabase = await User.findOne({username: req.body.username})
-        if (userInDatabase) {
-            return res.send("Username already taken")
-        }
+  // Best practice is to use a try/catch block...
+  try {
+    // Checks if a user with this username already exists in our database...
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (userInDatabase) {
+      return res.send('Username already taken!')
+    }
+    // Makes sure our user's password and confirmPassword are matching...
     if (req.body.password !== req.body.confirmPassword) {
-        return res.send("both passwords dont match")
+      return res.send('Password and Confirm Password must match')
     }
+    // Sends the password to bcrypt to be hashed using 12 "salt rounds" (think, 'levels of complexity')...
     const hashedPassword = bcrypt.hashSync(req.body.password, 12)
-    req.body.password = hashedPassword
-    const user = await User.create(req.body)
-    res.send(`Thanks for signing up ${user.username} your ID is ${user._id}`)
-    } catch (error) {
-        console.error(error.message)   
-    }
+    // Finally, creates the new user in our database...
+    const user = await User.create({
+      username: req.body.username,
+      password: hashedPassword
+    })
+    res.send(`Thanks for signing up ${user.username}! Your ID is ${user._id}!`)
+  } catch (error) {
+    // If there is an error, sends back the details of the error...
+    console.error(error.message)
+  }
 })
 
+router.get('/sign-in', (req, res) => {
+  res.render('./auth/sign-in.ejs')
+})
 
+router.post('/sign-in', async (req, res) => {
+  try {
+    // See if the user exists in the database...
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (!userInDatabase) {
+      return res.send('Login failed. Please try again.')
+    }
+    // Compare user's password with encrypted pw in db...
+    const validPassword = bcrypt.compareSync(
+      req.body.password,
+      userInDatabase.password
+    )
+    if (!validPassword) {
+      return res.send("Login failed. Please try again.")
+    }
+    // Create the session for the client...
+    req.session.user = {
+      username: userInDatabase.username,
+      _id: userInDatabase._id
+    }
+    res.redirect("/")
+  } catch (error) {
+    console.error(error.message)
+  }
+})
 
-
+router.get("/sign-out", (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+  });  
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 const express = require('express')
 const logger = require('morgan')
+const methodOverride = require('method-override')
 require('dotenv').config()
+const session = require('express-session')
 
 const authController = require('./controllers/auth.js')
 
-const app = express();
+const app = express()
 
-const db = require('./db') //import db file
+const db = require('./db')
 
-const PORT = process.env.PORT ? process.env.PORT : "3000";
+const PORT = process.env.PORT ? process.env.PORT : 3000
 
-app.use(express.urlencoded({ extended: false}))
-
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 app.use(logger('dev'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use('/auth', authController)
 
-app.get("/", async (req, res) => {
-    res.render("./index.ejs")
+app.get('/', async (req, res) => {
+  res.render('./index.ejs', { user: req.session.user })
+})
+
+app.get('/vip-lounge', (req, res) => {
+    if (req.session.user) {
+        res.send(`Welcome to the party ${req.session.user.username}.`)
+    } else {
+        res.send("sorry, no guests allowed.")
+    }
 })
 
 app.listen(PORT, () => {
-    console.log(`Express App running on PORT ${PORT}...`)
+  console.log(`Express App running on Port ${PORT} . . . `)
 })
 
